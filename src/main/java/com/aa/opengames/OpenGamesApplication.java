c6d622c2499ff65c96f1ef1a50fd6e08b32ded91
Antonio Alonzi <antonioalonzi85@gmail.com>
2018-02-25 16:13:19 +0000

    #1 Added Maven Spring Boot Hello World.ˆ

---
+++ src/main/java/com/aa/opengames/OpenGamesApplication.java
@@ -0,0 +1,11 @@
+package com.aa.opengames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenGamesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OpenGamesApplication.class, args);
+    }
+}
