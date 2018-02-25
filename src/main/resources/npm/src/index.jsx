462163538dc68f04c6816fd398ee2091e56987e4
Antonio Alonzi <antonioalonzi85@gmail.com>
2018-02-25 19:18:05 +0000

    #3 Added Login routing: a refresh under /login breaks as spring tries to find /login under /static.ˆ

--- src/main/resources/npm/src/index.jsx
+++ src/main/resources/npm/src/index.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
 import {render} from 'react-dom';
+import {BrowserRouter as Router, Link, Route} from "react-router-dom";
+
+class Header extends React.Component {
+    render () {
+        return (
+            <div id="header">
+                <div id="header-left">
+                    <Link to="/">Open Games</Link>
+                </div>
+                <div id="header-right">
+                    <Link to="/login">Login</Link>
+                </div>
+            </div>
+        );
+    }
+}
 
 class App extends React.Component {
     render () {
-        return <p>Hello World!</p>;
+        return (
+            <Router>
+                <div>
+                    <Header/>
+                    <Route path="/login" component={Login} />
+                </div>
+            </Router>
+        );
+    }
+}
+
+class Login extends React.Component {
+    render() {
+        return (
+            <div>Login</div>
+        );
     }
 }
 
