06d32bc718c36e4f00a171cb027a4d2038fa1237
Antonio Alonzi <antonioalonzi85@gmail.com>
2018-02-26 21:54:49 +0000

    #3 Added lint.

--- src/main/resources/app/src/login/Login.spec.js
+++ src/main/resources/app/src/login/Login.spec.js
@@ -1,14 +1,14 @@
 import React from "react";
 import {Login} from "./Login";
-import {shallow} from 'enzyme';
+import {shallow} from "enzyme";
 
 describe("test login", () => {
-    it('should display login', () => {
+    it("should display login", () => {
         // Given
         const subject = shallow(<Login/>);
 
         // When
-        const loginDiv = subject.find('div');
+        const loginDiv = subject.find("div");
 
         // Then
         expect(loginDiv.text()).toEqual("Login");
