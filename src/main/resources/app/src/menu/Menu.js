726a08a724baf05c1d9037e98034428fa234d525
Antonio Alonzi <antonioalonzi85@gmail.com>
2018-04-25 20:09:22 +0100

    #20: responsive menu almost done.

--- src/main/resources/app/src/menu/Menu.js
+++ src/main/resources/app/src/menu/Menu.js
@@ -1,19 +1,29 @@
-import './gameMenu.css'
+import './menu.css'
 import React from 'react'
 import PropTypes from 'prop-types'
+import {NavLink} from 'react-router-dom'
 import {GamesMenu} from './GamesMenu'
 import {UsersMenu} from './UsersMenu'
 import {Utils} from '../utils/Utils'
 
 export class Menu extends React.Component {
+  constructor(props) {
+    super(props)
+    this.closeMenu = this.closeMenu.bind(this)
+  }
+
+  closeMenu() {
+    Utils.dispatchEvent("toggle-menu-event")
+  }
+
   render() {
     if (Utils.isMobile()) {
       if (this.props.menuMobileExpanded) {
         return (
           <div id="mobile-menu">
             <ul>
-              <li>Games</li>
-              <li>Users</li>
+              <li><NavLink className="header-link" to="/portal/games" onClick={this.closeMenu}>Games</NavLink></li>
+              <li><NavLink className="header-link" to="/portal/users" onClick={this.closeMenu}>Users</NavLink></li>
             </ul>
           </div>
         )
