aedcee1fc81c425991b6d88e78f0982b734f90c1
Antonio Alonzi <antonioalonzi85@gmail.com>
2018-04-29 19:06:34 +0100

    #19: TicTacToe scheme.

--- src/main/resources/app/src/games/tictactoe/TicTacToe.js
+++ src/main/resources/app/src/games/tictactoe/TicTacToe.js
@@ -14,11 +14,38 @@ export class TicTacToe extends React.Component {
   componentWillUnmount() {
   }
 
+  opponent() {
+    if (this.props.user.username === this.props.table.owner) {
+      return this.props.table.joiners[0]
+    } else {
+      return this.props.table.owner
+    }
+  }
+
   render() {
     return (
       <div>
-        {this.props.game.name}
-        {this.props.table.id}
+        <div id='tic-tac-toe-status'>
+          <span>{this.props.user.username}</span> vs <span>{this.opponent()}</span>
+        </div>
+
+        <div id='tic-tac-toe-scheme'>
+          <div className={'tic-tac-toe-row'}>
+            <div id={'tic-tac-toe-cell-11'} className={'tic-tac-toe-cell'} />
+            <div id={'tic-tac-toe-cell-12'} className={'tic-tac-toe-cell'} />
+            <div id={'tic-tac-toe-cell-13'} className={'tic-tac-toe-cell'} />
+          </div>
+          <div className={'tic-tac-toe-row'}>
+            <div id={'tic-tac-toe-cell-21'} className={'tic-tac-toe-cell'} />
+            <div id={'tic-tac-toe-cell-22'} className={'tic-tac-toe-cell'} />
+            <div id={'tic-tac-toe-cell-23'} className={'tic-tac-toe-cell'} />
+          </div>
+          <div className={'tic-tac-toe-row'}>
+            <div id={'tic-tac-toe-cell-31'} className={'tic-tac-toe-cell'} />
+            <div id={'tic-tac-toe-cell-32'} className={'tic-tac-toe-cell'} />
+            <div id={'tic-tac-toe-cell-33'} className={'tic-tac-toe-cell'} />
+          </div>
+        </div>
       </div>
     )
   }
